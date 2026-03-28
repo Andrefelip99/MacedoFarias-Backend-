01c5e1ff4861c9792f4b960b919b9bf74d7e0305
andre felipe <andre.felipe106@icloud.com>
2026-03-28 02:24:55 -0300

    comentei o preautorize

--- src/main/java/com/example/confeitariaMacedoFarias/controllers/OrderController.java
+++ src/main/java/com/example/confeitariaMacedoFarias/controllers/OrderController.java
@@ -4,7 +4,6 @@ package com.example.confeitariaMacedoFarias.controllers;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,7 +23,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/v1/orders")
 @RequiredArgsConstructor
-@PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+//@PreAuthorize("hasAnyRole('USER', 'ADMIN')")//
 public class OrderController {
     private final OrderService service;
 
